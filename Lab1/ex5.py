374114199e7365ea8df8961e79f99e8a004f04a9
Catalina Bazic <bazic.catalina@gmail.com>
2023-10-10 17:05:38 +0300

    :star:

--- Lab1/ex5.py
+++ Lab1/ex5.py
@@ -1,7 +1,5 @@
 
 
-
-
 matrix = [
     [1, 2, 3],
     [8, 9, 4],
@@ -9,5 +7,37 @@ matrix = [
 ]
 
 def SpiralMatrix(matrix):
-    for i in len(matrix):
-        for j in len(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    top = 0
+    bottom= rows - 1
+    left= 0
+    right= cols - 1
+    result=[]
+
+    while top <= bottom and left <= right:
+        # draepta
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+
+        # in jos
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+
+        # stanga
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+
+        # in sus
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+                left += 1
+        return result
+
+
+print("Matrix readed in spiral is:" , str(SpiralMatrix(matrix)))
